83fd87e52285e12e178e4cc6cf61520b6c07254f
Shivang Yadav <shivangyadav121@gmail.com>
2023-10-10 17:48:56 +0530

    CORS update

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ export const app = express();
 config({
   path: "./data/config.env",
 });
-app.use(express.json({ limit: "50mb" }));
-app.use(express.urlencoded({ limit: "50mb", extended: true }));
+app.use(express.json());
+app.use(express.urlencoded());
 app.use(cookieParser());
 app.use(
   cors({
     origin: [process.env.FRONTEND_URL],
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   }),
 );
