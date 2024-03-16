db63d277cc05127d4caf2ef14d6b7fc5f591149b
Shivang Yadav <125182653+shivang-16@users.noreply.github.com>
2024-03-16 14:47:29 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     credentials: true,
   }),
